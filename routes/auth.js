8c0578f3af283c1260d954eaca3f114e8b9d1536
Kyla Massey <sir.massey@gmail.com>
2017-01-29 22:08:52 -0500

    add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -15,3 +15,22 @@ router.post('/register', (req, res, next)  => {
   })
   .catch((err) => { res.status(500).json({ status: 'error' }); });
 });
+
+router.get('/login', authHelpers.loginRedirect, (req, res)=> {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
+
